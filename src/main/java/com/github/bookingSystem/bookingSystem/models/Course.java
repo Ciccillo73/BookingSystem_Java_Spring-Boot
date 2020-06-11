76952b729c970416ba2e0dd7d56ffcc727c9f7ff
Francesco <med.dama@gmail.com>
2020-06-11 11:11:50 +0100

    Create Course class

---
+++ src/main/java/com/github/bookingSystem/bookingSystem/models/Course.java
@@ -0,0 +1,23 @@
+package com.github.bookingSystem.bookingSystem.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "courses")
+
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column(name = "star_rating")
+    private int starRating;
+
+    @Column
+    private String town;
+
+}
