5aff8565d36d7b950839c8cf1f174f41f3b73f72
Francesco <med.dama@gmail.com>
2020-06-11 12:12:05 +0100

    Create Booking repository

---
+++ src/main/java/com/github/bookingSystem/bookingSystem/repositories/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.github.bookingSystem.bookingSystem.repositories;
+
+import com.github.bookingSystem.bookingSystem.models.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends JpaRepository <Booking, Long> {
+}
