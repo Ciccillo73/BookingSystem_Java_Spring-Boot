89ce22b8e4999403ffd98f3aaab4fbd2030f5b54
Francesco <med.dama@gmail.com>
2020-06-11 14:20:30 +0100

    Create query findByCustomerCourseName

--- src/main/java/com/github/bookingSystem/bookingSystem/repositories/CustomerRepository.java
+++ src/main/java/com/github/bookingSystem/bookingSystem/repositories/CustomerRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 @Repository
 public interface CustomerRepository extends JpaRepository <Customer, Long> {
 
-    List<Customer> findByBookingCourseName(String courseName);
+    List<Customer> findByBookingsCourseName(String name);
 
 }
--- src/test/java/com/github/bookingSystem/bookingSystem/BookingSystemApplicationTests.java
+++ src/test/java/com/github/bookingSystem/bookingSystem/BookingSystemApplicationTests.java
@@ -1,7 +1,9 @@
 package com.github.bookingSystem.bookingSystem;
 
 import com.github.bookingSystem.bookingSystem.models.Course;
+import com.github.bookingSystem.bookingSystem.models.Customer;
 import com.github.bookingSystem.bookingSystem.repositories.CourseRepository;
+import com.github.bookingSystem.bookingSystem.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +19,9 @@ class BookingSystemApplicationTests {
 	@Autowired
 	CourseRepository courseRepository;
 
+	@Autowired
+	CustomerRepository customerRepository;
+
 	@Test
 	void contextLoads() {
 	}
@@ -27,6 +32,11 @@ class BookingSystemApplicationTests {
 		assertEquals(3, foundCourse.size());
 	}
 
+	@Test
+	public void canFindCustomerByCourseName(){
+		List<Customer> foundCustomer = customerRepository.findByBookingsCourseName("Java");
+		assertEquals(2, foundCustomer.size());
 
+	}
 
 }
