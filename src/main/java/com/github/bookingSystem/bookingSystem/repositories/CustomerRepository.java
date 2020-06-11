b58c6730cf96e7b958529096f0139c50d7d240e6
Francesco <med.dama@gmail.com>
2020-06-11 12:11:06 +0100

    Create Customer repository

---
+++ src/main/java/com/github/bookingSystem/bookingSystem/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.github.bookingSystem.bookingSystem.repositories;
+
+import com.github.bookingSystem.bookingSystem.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository <Customer, Long> {
+}
